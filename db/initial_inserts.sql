143c0c40fcc6ecbcf23ad4f3a386632355ea5d0d
= <=>
2025-08-13 13:17:59 -0300

    alterações no arquivo de initial_inserts.sql

--- db/initial_inserts.sql
+++ db/initial_inserts.sql
@@ -1,7 +1,9 @@
-INSERT INTO usuario(email, login, senha) VALUES ('paulo.takeda@gmail.com', 'ptakeda', '123456');
-
 INSERT INTO tipo_carteira(id_tipo_carteira, nome) VALUES (1, 'Caixa');
 INSERT INTO tipo_carteira(id_tipo_carteira, nome) VALUES (2, 'Conta Corrente');
 INSERT INTO tipo_carteira(id_tipo_carteira, nome) VALUES (3, 'Conta Poupança');
 
--- INSERT INTO carteira(nome, id_tipo_carteira, id_usuario) VALUES ('Caixa', (SELECT id_tipo_carteira FROM tipo_carteira WHERE nome = 'Caixa'), (SELECT MIN(id_usuario) FROM usuario));
+INSERT INTO carteira(id_banco, nome, id_tipo_carteira, saldo_inicial, id_usuario) VALUES (NULL, 'Caixa', 1, 0, 1);
+INSERT INTO carteira(id_banco, nome, id_tipo_carteira, saldo_inicial, id_usuario) VALUES (57, 'Banco do Brasil - CC', 2, 0, 1);
+INSERT INTO carteira(id_banco, nome, id_tipo_carteira, saldo_inicial, id_usuario) VALUES (57, 'Banco do Brasil - CP', 3, 0, 1);
+INSERT INTO carteira(id_banco, nome, id_tipo_carteira, saldo_inicial, id_usuario) VALUES (141, 'Caixa Econômica - CC', 2, 0, 1);
+INSERT INTO carteira(id_banco, nome, id_tipo_carteira, saldo_inicial, id_usuario) VALUES (1400, 'NUBANK - CC', 2, 0, 1);
