6780501de8c6c3dfc4c24bf1aa27efb34553f0de
Paulo Roberto Schmitz Takeda <122681663+paulotakeda1986@users.noreply.github.com>
2025-08-19 12:24:17 -0300

    add

---
+++ db/009 - categoria_receita.sql
@@ -0,0 +1,7 @@
+INSERT INTO categoria_receita (id_categoria_receita, nome, fl_fixo, id_usuario) VALUES (1 ,'Ajuste', false, 1);
+INSERT INTO categoria_receita (id_categoria_receita, nome, fl_fixo, id_usuario) VALUES (2 ,'Salário Mensal', true, 1);
+INSERT INTO categoria_receita (id_categoria_receita, nome, fl_fixo, id_usuario) VALUES (3 ,'Vale Alimentação', true, 1);
+INSERT INTO categoria_receita (id_categoria_receita, nome, fl_fixo, id_usuario) VALUES (4 ,'Empréstimo pessoal', false, 1);
+INSERT INTO categoria_receita (id_categoria_receita, nome, fl_fixo, id_usuario) VALUES (5 ,'Adiantamento de férias', false, 1);
+INSERT INTO categoria_receita (id_categoria_receita, nome, fl_fixo, id_usuario) VALUES (6 ,'Prestação de serviços', false, 1);
+INSERT INTO categoria_receita (id_categoria_receita, nome, fl_fixo, id_usuario) VALUES (7 ,'Rendimentos', false, 1);
