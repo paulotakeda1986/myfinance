e5f2a4967a0028e2dd9350ab587bdff13edb0106
= <=>
2025-08-24 13:59:19 -0300

    alterações nos arquivos sql e inserção de chave de unicidade na tabela fatura_cartao_credito

--- db/005 - carteira.sql
+++ db/005 - carteira.sql
@@ -1,7 +1,7 @@
-INSERT INTO carteira(id_banco, nome, id_tipo_carteira, saldo_inicial, saldo_atual, id_usuario) VALUES (NULL, 'Caixa', 1, 0, 0, 1);
-INSERT INTO carteira(id_banco, nome, id_tipo_carteira, saldo_inicial, saldo_atual, id_usuario) VALUES (1, 'Banco do Brasil - CC', 2, 0, 0, 1);
-INSERT INTO carteira(id_banco, nome, id_tipo_carteira, saldo_inicial, saldo_atual, id_usuario) VALUES (1, 'Banco do Brasil - CP', 3, 0, 0, 1);
-INSERT INTO carteira(id_banco, nome, id_tipo_carteira, saldo_inicial, saldo_atual, id_usuario) VALUES (5, 'Caixa Econômica - CC', 2, 0, 0, 1);
-INSERT INTO carteira(id_banco, nome, id_tipo_carteira, saldo_inicial, saldo_atual, id_usuario) VALUES (19, 'NUBANK - CC', 2, 0, 0, 1);
-INSERT INTO carteira(id_banco, nome, id_tipo_carteira, saldo_inicial, saldo_atual, id_usuario) VALUES (NULL, 'Flash (Vale Alimentação)', 1, 0, 0, 1);
-INSERT INTO carteira(id_banco, nome, id_tipo_carteira, saldo_inicial, saldo_atual, id_usuario) VALUES (NULL, 'SINTINORP (DUCZ)', 1, 8.93, 8.93, 1);
+INSERT INTO carteira(id_carteira, id_banco, nome, id_tipo_carteira, saldo_inicial, saldo_atual, id_usuario) VALUES (1, NULL, 'Caixa', 1, 0, 0, 1);
+INSERT INTO carteira(id_carteira, id_banco, nome, id_tipo_carteira, saldo_inicial, saldo_atual, id_usuario) VALUES (2, 1, 'Banco do Brasil - CC', 2, 0, 0, 1);
+INSERT INTO carteira(id_carteira, id_banco, nome, id_tipo_carteira, saldo_inicial, saldo_atual, id_usuario) VALUES (3, 1, 'Banco do Brasil - CP', 3, 0, 0, 1);
+INSERT INTO carteira(id_carteira, id_banco, nome, id_tipo_carteira, saldo_inicial, saldo_atual, id_usuario) VALUES (4, 5, 'Caixa Econômica - CC', 2, 0, 0, 1);
+INSERT INTO carteira(id_carteira, id_banco, nome, id_tipo_carteira, saldo_inicial, saldo_atual, id_usuario) VALUES (5, 19, 'NUBANK - CC', 2, 0, 0, 1);
+INSERT INTO carteira(id_carteira, id_banco, nome, id_tipo_carteira, saldo_inicial, saldo_atual, id_usuario) VALUES (6, NULL, 'Flash (Vale Alimentação)', 1, 0, 0, 1);
+INSERT INTO carteira(id_carteira, id_banco, nome, id_tipo_carteira, saldo_inicial, saldo_atual, id_usuario) VALUES (7, NULL, 'SINTINORP (DUCZ)', 1, 8.93, 8.93, 1);
